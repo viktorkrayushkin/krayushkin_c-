2be1ca90c94239e585e717d21b6742081390cb36
Viktor Krayushkin <100699441+viktorkrayushkin@users.noreply.github.com>
2022-09-09 15:03:04 +0300

    Add 3_ex

---
+++ 3_ex.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{ int a,b,c,x1,x2,D;
+    std::cout <<"Введите коэфициент a, b, c: ";
+    std:: cin >> a >> b >> c ;
+
+    D = b*b - 4*a*c;
+    if (D > 0) {
+        x1 = ((-b) + sqrt(D)) / (2 * a);
+        x2 = ((-b) - sqrt(D)) / (2 * a);
+        std::cout << "x1 = " << x1 << "\n";
+        std::cout << "x2 = " << x2 << "\n";
+    }
+    if (D == 0){x1 = -(b / (2 * a));
+        std::cout << "x1 = x2 = " << x1 << "\n";
+    }
+    if (D < 0){
+       std:: cout << "Решения в действительных значениях уравнения не существует";
+}
+
+    return 0;
+
+}
